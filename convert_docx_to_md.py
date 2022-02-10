68a6fe14bb5ae2266f64d70038042088f3d2bd5a
luca.magnasco <luca.magnasco@accenture.com>
2022-02-10 13:04:28 -0300

    agregue convert_docx_to_md.py

---
+++ convert_docx_to_md.py
@@ -0,0 +1,6 @@
+import mammoth
+
+with open("LucaMagnasco_CV_en.docx", "rb") as docx_file:
+    result = mammoth.convert_to_markdown(docx_file)
+with open("README.md", "w") as markdown_file:
+    markdown_file.write(result.value)
